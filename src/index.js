8c7a2db5e59c8ddc0ce5bfdfd3f52b53c55c6757
ponyesteves <ajesteves@gmail.com>
2017-11-03 11:26:58 -0300

    allow origin

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import * as Rx from 'rxjs'
 // const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT1Bw7xBeaavCkQHjsciGovUZwOFZQuE4h-Hj4MCO5pY-Dl3fqiHpP9M0KavP_6gvf9uuBDcH1y-SFq/pub?gid=0&single=true&output=tsv'
 
 const getAndParse = (url) => {
+  console.log("HOLA")
   return Rx.Observable.create(obs => {
-    request(url, (e, r, b) => {
+    request({
+      url,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }, (e, r, b) => {
       if (e)
         return obs.error(e)
       obs.next(parse(b))
@@ -15,11 +21,11 @@ const getAndParse = (url) => {
 
 const parse = (tsv) => {
   return tsv.split('\r\n').map(row => row.split('\t')).map(_objWithFirstAsHeader).slice(1)
-} 
+}
 
 const _objWithFirstAsHeader = (rowAry) => {
   _objWithFirstAsHeader.header = _objWithFirstAsHeader.header || rowAry
   return rowAry.reduce((p, c, i) => ({ [_objWithFirstAsHeader.header[i]]: c, ...p }), {})
 }
 
-export { getAndParse, parse}
+export { getAndParse, parse }
